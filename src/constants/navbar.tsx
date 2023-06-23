a699bf4942ee0f00b8162a0a2ad3aba2d8f66c30
laneazzi <lanreayedunjr@gmail.com>
2023-06-22 21:55:04 -0500

    icons

--- src/constants/navbar.tsx
+++ src/constants/navbar.tsx
@@ -1,10 +1,17 @@
-import "./navbar.css";
+import homeLogo from "../assets/home.svg";
 import githubLogo from "../assets/github.svg";
 import resumeLogo from "../assets/resume.svg";
+import messageLogo from "../assets/message.svg";
+import questionLogo from "../assets/question.svg";
 import linkedinLogo from "../assets/linkedin.svg";
+import homeLightLogo from "../assets/homeLight.svg";
 import githubLightLogo from "../assets/githubLight.svg";
 import resumeLightLogo from "../assets/resumeLight.svg";
+import messageLightLogo from "../assets/messageLight.svg";
+import questionLightLogo from "../assets/questionLight.svg";
 import linkedinLightLogo from "../assets/linkedinLight.svg";
+
+import "./navbar.css";
 
 const Navbar = () => {
   const isLightModeEnabled = window.matchMedia(
@@ -18,7 +25,6 @@ const Navbar = () => {
           <a
             href="https://www.linkedin.com/in/olanrewaju-ayedun-88726b1b6/"
             target="_blank"
-            title="My Linkedin"
           >
             <img
               src={isLightModeEnabled ? linkedinLogo : linkedinLightLogo}
@@ -27,11 +33,7 @@ const Navbar = () => {
           </a>
         </li>
         <li>
-          <a
-            href="https://github.com/laneazzi"
-            target="_blank"
-            title="My Github"
-          >
+          <a href="https://github.com/laneazzi" target="_blank">
             <img
               src={isLightModeEnabled ? githubLogo : githubLightLogo}
               className="navbar-icons"
@@ -42,7 +44,7 @@ const Navbar = () => {
           <a
             href="https://drive.google.com/file/d/1_Hetf-sf1k8w2_65x1Q60Ug1wgY9ZWRh/view?usp=drive_link"
             target="_blank"
-            title="My Resume"
+            title="My resume"
           >
             <img
               src={isLightModeEnabled ? resumeLogo : resumeLightLogo}
@@ -53,10 +55,32 @@ const Navbar = () => {
       </ul>
       <ul className="list">
         <li>
-          <a href="/">Home</a>
+          <a href="/">
+            <img
+              src={isLightModeEnabled ? homeLogo : homeLightLogo}
+              className="navbar-icons"
+            />
+          </a>
         </li>
         <li>
-          <a href="/contact">Contact</a>
+          <a href="/contact" title="Contact me">
+            <img
+              src={isLightModeEnabled ? messageLogo : messageLightLogo}
+              className="navbar-icons"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.uscis.gov/working-in-the-united-states/students-and-exchange-visitors/optional-practical-training-extension-for-stem-students-stem-opt"
+            title="STEM-OPT"
+            target="_blank"
+          >
+            <img
+              src={isLightModeEnabled ? questionLogo : questionLightLogo}
+              className="navbar-icons"
+            />
+          </a>
         </li>
       </ul>
     </nav>
